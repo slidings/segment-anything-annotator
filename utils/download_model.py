be1fdec9b681e2006e9aa6ba26fe7eb4a86578d8
ryan <ryanong0@gmail.com>
2026-03-09 17:00:13 +0800

    added the ignore and model link

--- utils/download_model.py
+++ utils/download_model.py
@@ -12,6 +12,7 @@ def download_model(model_type):
         "vit_h": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth",
         "vit_l": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth",
         "vit_b": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth",
+        "medsam_vit_b":"https://drive.google.com/uc?export=download&id=1UAmWL88roYR7wKlnApw5Bcuzf2iQgk6_",
     }
 
     # Check if the model file already exists and model_type is in model_urls
